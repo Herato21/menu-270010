786a2e839c11bcff8d9d4d73255e4cac4a73b645
cyanfox-d <cyanfox_d@hotmail.com>
2023-10-24 23:21:18 -0700

    Se agregaron y editaron los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Inicio', url: '/inicio', icon: 'home' },
+    { title: 'Galería', url: '/galeria', icon: 'images' },
+    { title: 'Nosotros', url: '/nosotros', icon: 'people-circle' },
+    { title: 'Contacto', url: '/contacto', icon: 'call' },
+    { title: 'Perfil', url: '/folder/trash', icon: 'person-circle' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
