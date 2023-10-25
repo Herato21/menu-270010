ec79deb742e626c7b6f9b1bce6a832f49028f12f
cyanfox-d <cyanfox_d@hotmail.com>
2023-10-24 23:02:37 -0700

    Se añadio pagina galeria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'galeria',
+    loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
   }
 ];
 
